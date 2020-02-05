772264d315460b325372a4cc6ba9169063090d04
Sam Lanning <sam@samlanning.com>
2020-02-05 20:08:01 +0000

    Connect museeks to synesthesia (without meta)

--- src/ui/lib/player.ts
+++ src/ui/lib/player.ts
@@ -1,3 +1,6 @@
+import { ControllerEndpoint } from '@synesthesia-project/core/lib/protocols/control';
+import { DEFAULT_SYNESTHESIA_PORT } from '@synesthesia-project/core/lib/constants';
+
 import * as app from './app';
 
 interface PlayerOptions {
@@ -11,6 +14,7 @@ class Player {
   private audio: HTMLAudioElement;
   private durationThresholdReached: boolean;
   public threshold: number;
+  private synesthesiaEndpoint: Promise<ControllerEndpoint> | null = null;
 
   constructor(options?: PlayerOptions) {
     const mergedOptions = {
@@ -33,6 +37,92 @@ class Player {
 
     this.threshold = 0.75;
     this.durationThresholdReached = false;
+
+    // Initialize Synesthesia
+    this.updateSynesthesiaState = this.updateSynesthesiaState.bind(this);
+    this.audio.addEventListener('playing', this.updateSynesthesiaState);
+    this.audio.addEventListener('pause', this.updateSynesthesiaState);
+    this.audio.addEventListener('seeked', this.updateSynesthesiaState);
+  }
+
+  private getSynesthesiaEndpoint() {
+    if (!this.synesthesiaEndpoint) {
+      const endpointPromise =
+        this.synesthesiaEndpoint =
+        new Promise((resolve, reject) => {
+          const ws = new WebSocket(`ws://localhost:${DEFAULT_SYNESTHESIA_PORT}/control`);
+          const endpoint = new ControllerEndpoint(msg => ws.send(JSON.stringify(msg)));
+          ws.addEventListener('open', () => {
+            endpoint.setRequestHandler(async req => {
+              if (!this.audio) return { success: false };
+              switch (req.request) {
+                case 'pause':
+                  this.audio.pause();
+                  return { success: true };
+                case 'toggle':
+                  this.audio.paused ? this.audio.play() : this.audio.pause();
+                  return { success: true };
+                case 'go-to-time':
+                  this.audio.currentTime = req.positionMillis / 1000;
+                  return { success: true };
+                case 'play-speed':
+                  this.audio.playbackRate = req.playSpeed;
+                  return { success: true };
+              }
+            });
+            resolve(endpoint);
+          });
+          ws.addEventListener('error', err => {
+            if (endpointPromise === this.synesthesiaEndpoint)
+              this.synesthesiaEndpoint = null;
+            reject(err);
+          });
+          ws.addEventListener('close', _ => {
+            if (endpointPromise === this.synesthesiaEndpoint)
+              this.synesthesiaEndpoint = null;
+          });
+          ws.addEventListener('message', msg => {
+            endpoint.recvMessage(JSON.parse(msg.data));
+          });
+        });
+
+        this.synesthesiaEndpoint.catch(err => {
+          console.error(err);
+          if (this.synesthesiaEndpoint === endpointPromise) {
+            // Remove the endpoint so an attempt will be tried again
+            this.synesthesiaEndpoint = null;
+          }
+        });
+      }
+
+    return this.synesthesiaEndpoint;
+  }
+
+  private updateSynesthesiaState() {
+    this.getSynesthesiaEndpoint().then(endpoint => {
+      if (/*!this.meta ||*/ !this.audio) return;
+      endpoint.sendState({
+        layers: [{
+          // TODO: optionally send file path instead of meta
+          file: {
+            type: 'meta' as 'meta',
+            title: 'dummy',//this.meta.title,
+            artist: 'dummy',//this.meta.artist,
+            album: 'dummy',//this.meta.album,
+            lengthMillis: this.audio.duration * 1000
+          },
+          state: this.audio.paused ? {
+            type: 'paused',
+            positionMillis:
+              this.audio.currentTime * 1000
+          } : {
+              type: 'playing',
+              effectiveStartTimeMillis: new Date().getTime() - this.audio.currentTime * 1000 / this.audio.playbackRate,
+              playSpeed: this.audio.playbackRate
+            }
+        }]
+      });
+    });
   }
 
   async play() {
